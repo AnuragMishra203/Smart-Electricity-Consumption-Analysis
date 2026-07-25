9849c949dc08bb9a5bced008b677a484a52d9b8b
AnuragMishra203 <anurag.rkl203@gmail.com>
2026-07-25 23:51:03 +0530

    Update eda.py

--- eda.py
+++ eda.py
@@ -1,11 +1,1 @@
-def dataset_summary(df):
-    print(df.describe())
-
-def missing_values(df):
-    print(df.isnull().sum())
-
-def correlation_matrix(df):
-    print(df.corr(numeric_only=True))
-
-def duplicate_rows(df):
-    print(df.duplicated().sum())
+ 
